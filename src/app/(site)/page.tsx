0558e88c9ed1223409b936b6cb4453c967b5fdbc
Ichthus Mkt - Agência Win <contatoagenciawin@gmail.com>
2026-09-06 16:34:41 -0300

    feat: complete home closing sections

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,11 +1,15 @@
+import { ContributionSection } from "@/components/home/ContributionSection";
 import { FeaturedTerritorySection } from "@/components/home/FeaturedTerritorySection";
 import { FoundersSection } from "@/components/home/FoundersSection";
 import { HomeHero } from "@/components/home/HomeHero";
+import { ImpactCtaSection } from "@/components/home/ImpactCtaSection";
 import { ImpactSection } from "@/components/home/ImpactSection";
 import { MotionSection } from "@/components/home/MotionSection";
 import { OpportunitiesSection } from "@/components/home/OpportunitiesSection";
 import { TerritoriesSection } from "@/components/home/TerritoriesSection";
+import { VisualMosaicSection } from "@/components/home/VisualMosaicSection";
 import { WhoWeAre } from "@/components/home/WhoWeAre";
+import { SiteFooter } from "@/components/site/SiteFooter";
 import { SiteHeader } from "@/components/site/SiteHeader";
 
 export default function HomePage() {
@@ -20,6 +24,10 @@ export default function HomePage() {
       <MotionSection />
       <FoundersSection />
       <OpportunitiesSection />
+      <ImpactCtaSection />
+      <VisualMosaicSection />
+      <ContributionSection />
+      <SiteFooter />
     </main>
   );
 }
