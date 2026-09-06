75661389cb913c36a84719bf5860589e326ecb26
Ichthus Mkt - Agência Win <contatoagenciawin@gmail.com>
2026-09-06 14:46:54 -0300

    feat: render hero variants for comparison

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import { HomeHero } from "@/components/home/HomeHero";
+import { HomeHero2 } from "@/components/home/HomeHero2";
 import { SiteHeader } from "@/components/site/SiteHeader";
 
 export default function HomePage() {
@@ -6,6 +7,7 @@ export default function HomePage() {
     <main className="min-h-screen bg-[var(--fontes-off-white)] text-[var(--fontes-black)]">
       <SiteHeader />
       <HomeHero />
+      <HomeHero2 />
     </main>
   );
 }
