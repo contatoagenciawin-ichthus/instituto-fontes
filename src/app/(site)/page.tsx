2a5c29f51fae7762bcca08f9a622a4ca71cd5b2b
Ichthus Mkt - Agência Win <contatoagenciawin@gmail.com>
2026-09-06 14:52:59 -0300

    chore: lock approved hero variant

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,5 +1,4 @@
 import { HomeHero } from "@/components/home/HomeHero";
-import { HomeHero2 } from "@/components/home/HomeHero2";
 import { SiteHeader } from "@/components/site/SiteHeader";
 
 export default function HomePage() {
@@ -7,7 +6,6 @@ export default function HomePage() {
     <main className="min-h-screen bg-[var(--fontes-off-white)] text-[var(--fontes-black)]">
       <SiteHeader />
       <HomeHero />
-      <HomeHero2 />
     </main>
   );
 }
